24e2e95f4506b0cf4e6d0c3009b777f05036b89d
danildperez04 <gamerdan589@gmail.com>
2026-08-06 13:40:31 -0600

    feat: add users variety and health centers - Patient - HealthcareWorker - Caregiver - HealthCenter
    and its relationships

---
+++ api/src/features/users/entities/patient.entity.ts
@@ -0,0 +1,52 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+import { Genre } from '../../catalogues/entities/genre.entity';
+import { HealthCenter } from '../../health-centers/entities/health-center.entity';
+
+@Entity('patient')
+export class Patient {
+  @PrimaryColumn('uuid')
+  id!: string;
+
+  @Column({ name: 'date_of_birth', type: 'date' })
+  dateOfBirth!: Date;
+
+  @Column({ name: 'genre_id' })
+  genreId!: number;
+
+  @Column({ name: 'health_center_id', type: 'uuid' })
+  healthCenterId!: string;
+
+  @Column({ name: 'emergency_contact_name' })
+  emergencyContactName!: string;
+
+  @Column({ name: 'emergency_contact_phone_number' })
+  emergencyContactPhoneNumber!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'id', referencedColumnName: 'id' })
+  user!: User;
+
+  @ManyToOne(() => Genre)
+  @JoinColumn({ name: 'genre_id' })
+  genre!: Genre;
+
+  @ManyToOne(() => HealthCenter, (healthCenter) => healthCenter.patients)
+  @JoinColumn({ name: 'health_center_id' })
+  healthCenter!: HealthCenter;
+}
