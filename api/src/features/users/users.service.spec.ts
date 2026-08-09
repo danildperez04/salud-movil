27ccd9194e61895b3b68a006ab958ae2ecef97a6
danildperez04 <gamerdan589@gmail.com>
2026-08-09 14:44:20 -0600

    feat(api): add admin users CRUD for health staff (HU-01)

--- api/src/features/users/users.service.spec.ts
+++ api/src/features/users/users.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getDataSourceToken, getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { HealthcareWorker } from './entities/healthcare-worker.entity';
+import { Role } from '../catalogues/entities/role.entity';
+import { Municipality } from '../catalogues/entities/municipality.entity';
+import { Major } from '../catalogues/entities/major.entity';
+import { HealthCenter } from '../health-centers/entities/health-center.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: {} },
+        { provide: getRepositoryToken(HealthcareWorker), useValue: {} },
+        { provide: getRepositoryToken(Role), useValue: {} },
+        { provide: getRepositoryToken(Municipality), useValue: {} },
+        { provide: getRepositoryToken(Major), useValue: {} },
+        { provide: getRepositoryToken(HealthCenter), useValue: {} },
+        { provide: getDataSourceToken(), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
