b4d72fc8939b511a5fe2b6a7bd0f0bfad8fb60e3
Jarey-17 <Julio.Reyes35n@std.uni.edu.ni>
2026-08-12 13:22:57 -0600

    chore: configure eslint, prettier, husky and lint-staged

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,14 +1,14 @@
-import "../global.css";
-import { Stack } from "expo-router";
-import { ThemeProvider } from "expo-router/react-navigation";
-import { PortalHost } from "@rn-primitives/portal";
-import { useColorScheme } from "react-native";
-import { NAV_THEME } from "../lib/theme";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import '../global.css';
+import { Stack } from 'expo-router';
+import { ThemeProvider } from 'expo-router/react-navigation';
+import { PortalHost } from '@rn-primitives/portal';
+import { useColorScheme } from 'react-native';
+import { NAV_THEME } from '../lib/theme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const theme = colorScheme === "dark" ? NAV_THEME.dark : NAV_THEME.light;
+  const theme = colorScheme === 'dark' ? NAV_THEME.dark : NAV_THEME.light;
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
